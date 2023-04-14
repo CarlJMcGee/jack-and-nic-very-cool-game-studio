f90c7af4affb09cf40c6f079d69d09010321057f
EverybodyMakesFsteaks <130267104+EverybodyMakesFsteaks@users.noreply.github.com>
2023-04-14 16:54:44 -0500

    Commenting for Cool Cat 1st Draft
    Finished commenting my code.
    In future, I will also track better when I did in the editor itself and inlcude it in code commenting in a section.
    
    Also, at the bottom, I included code that did not work with my explinations of why I tried it and how I understand it. I think it is also worth looking at.

--- cool-cat-corona/Characters/mask.gd
+++ cool-cat-corona/Characters/mask.gd
@@ -1,10 +1,16 @@
 extends Area2D
+# Export var : PackedScene makes an empty box in editor you can drag a scene to.
+#This lets you store a scene you want to make later.
 @export var Victory : PackedScene
 
+# Signals is how you get seperate scripts to talk to each other.
 signal Music_stop
 
 
-
+# When Mask's hitbox collides with any other hitbox,
+# emit() is how you send a signal to any script it is connected to.
+# Play victory song.
+# Create stored victory image.
 func _on_area_entered(area):
 	Music_stop.emit()
 	$VictorySong.play()
@@ -12,6 +18,9 @@ func _on_area_entered(area):
 	get_parent().add_child(win)
 	
 
-
+# Quit game when song finishes.
+"""
+# NOTE: REMEMBER TO CHANGE THIS IF WE EVER CONNECT THIS GAME TO OTHER MINI GAMES 
+"""
 func _on_victory_song_finished():
 	get_tree().quit()
--- cool-cat-corona/Levels/coolcat_level.gd
+++ cool-cat-corona/Levels/coolcat_level.gd
@@ -1,12 +1,27 @@
 extends Node2D
-#@export var covid_scene: PackedScene
-
-#Crashed my computer func _on_timer_timeout():
-	#var covid_spawn_location = $Glockmaster5000/Marker2D.position
-	#var covid = covid_scene.instance()
-	#add_child(covid)
-	#covid.poisition = covid_spawn_location
 
 
+#Stops the music when Cool Cat overlaps with maks, which sends a "signal" which 
+#allows other scripts to do things in response
 func _on_mask_music_stop():
 	$AudioStreamPlayer2D.stop()
+
+"""
+The commenting graveyard pit of failed attempts with reasoning behind it.
+
+# Thought I needed to load Covid into main scene to use it. I ended up loading
+it into the Glockmaster 5000.
+
+@export var covid_scene: PackedScene
+
+#Crashed my computer
+#Tried to make covid appear ever few seconds, and spawn it at a marker on
+the Glock5000. 
+	func _on_timer_timeout():
+		var covid_spawn_location = $Glockmaster5000/Marker2D.position
+		var covid = covid_scene.instance()
+		add_child(covid)
+		covid.poisition = covid_spawn_location
+
+
+"""
