5f49b9baf307b0e073e8ceb85af866926429d79a
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2023-05-06 14:15:33 -0500

    added bg img

--- Shit Screen The Game/MainMenu.gd
+++ Shit Screen The Game/MainMenu.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+var minigames := {
+	"cool_cat_corona": preload("res://cool cat corona/Levels/coolcat_level.tscn"),
+	"cool_cat_crossing": preload("res://cool cat crossing/main.tscn")
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +17,8 @@ func _process(delta):
 
 
 func _on_cool_cat_btn_pressed():
-	get_tree().change_scene_to_file('cool cat crossing/main.tscn')
+	get_tree().change_scene_to_packed(minigames['cool_cat_crossing'])
 
 func _on_corona_btn_pressed():
-	get_tree().change_scene_to_file('cool cat corona/Levels/coolcat_level.tscn')
+	get_tree().change_scene_to_packed(minigames["cool_cat_corona"])
+	
