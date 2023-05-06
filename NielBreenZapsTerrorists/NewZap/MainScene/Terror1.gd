1fb7bbb9a9e64e9959c66b64c36dd8da1d7526ab
EverybodyMakesFsteaks <130267104+EverybodyMakesFsteaks@users.noreply.github.com>
2023-05-06 15:17:27 -0500

    FIrst working deleting terrorist
    Rectangles are evil.

--- NielBreenZapsTerrorists/NewZap/MainScene/Terror1.gd
+++ NielBreenZapsTerrorists/NewZap/MainScene/Terror1.gd
@@ -4,5 +4,15 @@ signal winning
 
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
-	winning.emit(winning)
-	queue_free()
+	if Input.is_action_just_pressed("Click"):
+		winning.emit(winning)
+		queue_free()
+		print("Why no delete?")
+
+
+func _on_area_2d_mouse_entered():
+	print("You have entered DA ZONE")
+
+
+func _on_area_2d_mouse_exited():
+	print("Now leaving DA ZONE BYE BYE")
