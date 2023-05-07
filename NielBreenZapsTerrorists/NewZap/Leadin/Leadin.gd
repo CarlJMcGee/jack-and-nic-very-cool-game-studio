d6708dffd55eddfd9064c706ce8065e3c50f276a
EverybodyMakesFsteaks <130267104+EverybodyMakesFsteaks@users.noreply.github.com>
2023-05-06 21:11:11 -0500

    Text now appears in time

--- NielBreenZapsTerrorists/NewZap/Leadin/Leadin.gd
+++ NielBreenZapsTerrorists/NewZap/Leadin/Leadin.gd
@@ -2,9 +2,25 @@ extends Control
 
 @export var fullcountfull : float = 15
 @onready var fullcount = fullcountfull
+@onready var Text1 = $Text1
+@onready var Text2 = $Text2
+@onready var Text22 = $Text22
+@onready var Text3 = $Text3
+@onready var countdowntext = $countdowntext
+@onready var pic = $Mouse
+
+
 func _physics_process(_delta):
 	if fullcount == (fullcountfull - 1):
-		
+		Text1.visible = true
+	elif fullcount == (fullcountfull - 4 ):
+		Text2.visible = true
+	elif fullcount == (fullcountfull -2):
+		pic.visible = true
+	elif fullcount == (fullcountfull -5):
+		Text22.visible = true
+	elif fullcount == (fullcountfull - 7):
+		Text3.visible = true
 
 func _on_timer_timeout():
 	fullcount -= 1
