0d265851acf36e7994f196f117fef95f4f305bfe
EverybodyMakesFsteaks <130267104+EverybodyMakesFsteaks@users.noreply.github.com>
2023-05-17 22:47:25 -0500

    Made Robo arm rotate and Zap image

---
+++ Shit Screen The Game/Armcannon.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var _smoothed_mouse_pos: Vector2
+@export var flash_speed := 5
+@export var fade := false
+var time := 0
+var _visible = false
+
+func _physics_process(delta: float):
+	_smoothed_mouse_pos = lerp(_smoothed_mouse_pos, get_global_mouse_position(), 1)
+	look_at(_smoothed_mouse_pos)
+	
+func zapnow():
+	$Zap.visible = true
+	await get_tree().create_timer(0.7).timeout
+	$Zap.visible = false
+		
