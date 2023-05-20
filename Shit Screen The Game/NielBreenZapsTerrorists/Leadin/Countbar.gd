40b57712bfbe9c780410db5dc771bc763d985fc9
EverybodyMakesFsteaks <130267104+EverybodyMakesFsteaks@users.noreply.github.com>
2023-05-20 15:59:03 -0500

    Made my first particle effect of flame and smoke_also a bomb

--- Shit Screen The Game/NielBreenZapsTerrorists/Leadin/Countbar.gd
+++ Shit Screen The Game/NielBreenZapsTerrorists/Leadin/Countbar.gd
@@ -1,20 +1,6 @@
 extends TextureProgressBar
-@onready var barsize := 3
-
-var starttime : bool = false
 
 
-
-
-func _physics_process(_delta):
-	pass
 
 func _on_timer_timeout():
-	if starttime == true:
-		if value > 0.9:
-			value -= 1
-
-
-func _on_control_now():
-	starttime = true
-	visible = true
+	pass # Replace with function body.
