f5289a188b4dcc9467cdc51b23b650d5972e74d9
EverybodyMakesFsteaks <130267104+EverybodyMakesFsteaks@users.noreply.github.com>
2023-07-24 19:30:10 -0500

    Added ending scenes and tried to disable debuging visability

--- Shit Screen The Game/ShrekRetold/Level/shreklevel_1.gd
+++ Shit Screen The Game/ShrekRetold/Level/shreklevel_1.gd
@@ -1,14 +1,23 @@
 extends Node2D
 
+#Note: in Debug, you can turn on visible Paths and Navigation to see the path finding.
 
+var stopdouble = false
 #Replace print() with actual victory stuff
 func _on_exit_body_entered(body):
 	if body.is_in_group("Player"):
-		print("You win!")
+		if not stopdouble:
+			stopdouble = true
+			$Win.visible = true
 		
 
 
 func _on_shrek_dead():
-	print("LOSING TIME")
+	losingtime()
+
+func losingtime():
+	if not stopdouble:
+		stopdouble = true
+		$Lose.visible = true
 
 	
